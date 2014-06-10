45a48b6e81f764fbfdb050f4aa789768fd04c94c
Lukas Oppermann <lukas@vea.re>
2014-06-10 15:31:57 +0200

    bugfix for toggle of labels with space in name

--- layouts/default/default.js
+++ layouts/default/default.js
@@ -124,10 +124,10 @@ function selectMilestone(id){
 }
 
 function toggleLabel(id){
-	l = $('.label[data-label=' + id + "]");
+	l = $('.label[data-label="' + id + '"]');
 	if(id == 'None'){
-		$('.label').removeClass('active')
-		l.addClass('active')
+		$('.label').removeClass('active').find('a').css('color','');
+		l.addClass('active');
 	} else {
 		if(l.hasClass('active')){
 			l.removeClass('active');
@@ -181,9 +181,13 @@ function commentCount(comments){
 }
 
 function getContrastYIQ(hexcolor){
-	var r = parseInt(hexcolor.substr(0,2),16);
-	var g = parseInt(hexcolor.substr(2,2),16);
-	var b = parseInt(hexcolor.substr(4,2),16);
-	var yiq = ((r*299)+(g*587)+(b*114))/1000;
-	return (yiq >= 128) ? 'black' : 'white';
+	if( hexcolor !== undefined && typeof hexcolor === "string" )
+	{
+		var r = parseInt(hexcolor.substr(0,2),16);
+		var g = parseInt(hexcolor.substr(2,2),16);
+		var b = parseInt(hexcolor.substr(4,2),16);
+		var yiq = ((r*299)+(g*587)+(b*114))/1000;
+		return (yiq >= 128) ? 'black' : 'white';
+	}
+	return 'black';
 }
